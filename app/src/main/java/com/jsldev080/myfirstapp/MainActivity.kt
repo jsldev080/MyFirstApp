6d83b1e4b6d11faea24d4289ae20646dca85b5f1
jslit <jslit@jungsoo080ui-MacBookPro.local>
2023-04-26 19:29:13 +0900

    counter app

--- app/src/main/java/com/jsldev080/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/jsldev080/myfirstapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.jsldev080.myfirstapp // myfirstapp.jsldev080.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 // MainActivity class is inherited by AppCompatActivity
 class MainActivity : AppCompatActivity() { // Activity = Screen
@@ -9,6 +12,17 @@ class MainActivity : AppCompatActivity() { // Activity = Screen
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) // res - layout - activity_main.xml
         // press command key on activity_main to click
-        // activity_main.xml: there are Code section other than design one.
+        // activity_main.xml: there are Code section other than Design one.
+
+        // check out the Component Tree in activity_main.xml - Design
+        val btnClickMe = findViewById<Button>(R.id.mybutton) // Palette - Common - Button / Declared Attributes - id 'mybutton'
+        val tvMyTextView = findViewById<TextView>(R.id.textView) // Palette - Common - TextView / Declared Attributes - id 'textView'
+        var timesClicked = 0
+        btnClickMe.setOnClickListener { // each time we click on the Button (using method setOnClickListener of Button),
+            timesClicked += 1 // the number increases by one starting from zero
+            tvMyTextView.text = timesClicked.toString() // the text of the TextView changes to increased number ("0" -> "1" -> "2" -> "3" -> ...)
+            Toast.makeText(this, "Hey Jungsoo!", Toast.LENGTH_LONG).show() // text "Hey Jungsoo!" shows up on the screen
+        }
+
     }
 }
